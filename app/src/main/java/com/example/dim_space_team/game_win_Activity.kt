d188403307254d042e1756ef0b67c7db13843ad5
Galypso-Unreal <johann.moser0681@gmail.com>
2022-01-04 10:01:22 +0100

    update 2

---
+++ app/src/main/java/com/example/dim_space_team/game_win_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.dim_space_team
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class game_win_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_win)
+    }
+}
---
+++ app/src/main/java/com/example/dim_space_team/in_room_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.dim_space_team
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class in_room_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.in_room)
+    }
+}
