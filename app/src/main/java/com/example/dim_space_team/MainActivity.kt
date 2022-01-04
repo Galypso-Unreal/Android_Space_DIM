d36e0048b29f7211dc11810721c1c43e8a133f0f
Galypso-Unreal <johann.moser0681@gmail.com>
2022-01-04 13:15:19 +0100

    update 4

--- app/src/main/java/com/example/dim_space_team/MainActivity.kt
+++ app/src/main/java/com/example/dim_space_team/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.dim_space_team
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val playerBtn = findViewById<Button>(R.id.button_start)
+
+        playerBtn.setOnClickListener{
+            val intent = Intent(this, WainRoom::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/dim_space_team/WainRoom.kt
@@ -0,0 +1,13 @@
+package com.example.dim_space_team
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WainRoom : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.waiting_room)
+    }
+
+}
